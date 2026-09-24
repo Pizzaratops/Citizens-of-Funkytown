f8613e4eec8353a74245a7fbbe8f7e0e692f104c
Steakosaurus <93515926+Pizzaratops@users.noreply.github.com>
2026-09-24 22:32:01 +0200

    Update league-dues.js

--- data/league-dues.js
+++ data/league-dues.js
@@ -30,6 +30,8 @@ const LEAGUE_DUES_PAID = [
   { teamId: 9, season: '2026/27' },  // Greifswald SG Gerstensaft (Sven Kosanke)
   { teamId: 6, season: '2026/27' },  // Cook Island Airballers (Rocket Sascha)
   { teamId: 7, season: '2026/27' },  // Crackpistel Baller (Thies Rinner)
+  { teamId: 4, season: '2026/27' },  // Gewürz Jürgchens (Jürgen Willi)
+  { teamId: 5, season: '2026/27' },  // Team Peterson (Enno)
 ];
 
 // Rückgabe: "paid" | "owes" | "not-relevant"
